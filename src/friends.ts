93ab301eeb9b81dec407af18630429b5e0362f0e
Kariko Lin <caclx@outlook.com>
2024-09-27 12:49:11 +0800

    Update friends.ts
    approve suggestion of Viki:
    trailing whitespace of CJK, before ASCIIs.

--- src/friends.ts
+++ src/friends.ts
@@ -2,19 +2,19 @@ import { ThemeBlogHomeProjectOptions } from 'vuepress-theme-hope'
 
 const friends: ThemeBlogHomeProjectOptions[] = [
   {
-    name: "律回MelodyEcho",
+    name: "律回 MelodyEcho",
     icon: 'https://avatars.githubusercontent.com/u/75575271?v=4',
     desc: '可爱的 Python Dev 和音乐人律姐姐～',
     link: 'https://www.glowmem.com/',
   },
   {
-    name: '岛风Shimakaze',
+    name: '岛风 Shimakaze',
     icon: 'https://gravatar.loli.net/avatar/8aa3a373ab407b250e866cc7ed5deea8?s=400',
     desc: '前端后端通吃的全栈佬。项目很多，但多数生命周期都比较短。',
     link: 'https://frg2089.github.io/',
   },
   {
-    name: '神羽Snowykami',
+    name: '神羽 Snowykami',
     icon: 'https://q.qlogo.cn/g?b=qq&nk=2751454815&s=640',
     desc: 'Redrock 后端人员，主要维护轻雪的相关项目，时不时在 MC 里整活。非常厉害。',
     link: 'https://sfkm.me/'
